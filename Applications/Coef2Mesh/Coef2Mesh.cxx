3f09cdc8fd3b9c947f8896d1c4c7b646dfba46a5
Stefan Schlager <zarquon42@gmail.com>
2015-06-05 09:16:46 +0200

    improved Coef2Mesh.cxx

--- Applications/Coef2Mesh/Coef2Mesh.cxx
+++ Applications/Coef2Mesh/Coef2Mesh.cxx
@@ -21,10 +21,7 @@ using namespace neurolib ;
 
 
 static int degree, level ;
-static bool firstDer, secondDer, unit, normal, first, second, kappa, c, s, princDirs, princCurve, h, k, gradKappa, caustic ;
-static bool parFile ;
-static bool phiThetaFile ;
-static char* baseName ;
+static char* meshName ;
 
 // takes in a spharm coefficient file, and generates the evaluations of the first and second derivatives
 int main(int argc, const char **argv)
@@ -33,32 +30,15 @@ int main(int argc, const char **argv)
   char *coefFileName;
   
   coefFileName = ipGetStringArgument(argv, "-coef", NULL);  
-  baseName = ipGetStringArgument(argv, "-output", NULL);  
-  firstDer = ipExistsArgument (argv, "-d");
-  secondDer = ipExistsArgument (argv, "-dd");
+  meshName = ipGetStringArgument(argv, "-output", NULL);  
   degree = ipGetIntArgument (argv, "-deg", 12) ;
   level = ipGetIntArgument (argv, "-level", -1) ;
-  unit = ipExistsArgument ( argv, "-unit") ;
-  normal = ipExistsArgument (argv, "-norm") ;
-  first = ipExistsArgument (argv, "-I") ;
-  second = ipExistsArgument (argv, "-II") ;
-  kappa = ipExistsArgument (argv, "-kappa") ;
-  gradKappa = ipExistsArgument (argv, "-gradKappa") ;
-  caustic = ipExistsArgument (argv, "-caustic") ;
-  c = ipExistsArgument (argv, "-c") ;
-  s = ipExistsArgument (argv, "-s") ;
-  h = ipExistsArgument (argv, "-h") ;
-  k = ipExistsArgument (argv, "-k") ;
-
-  princDirs = ipExistsArgument (argv, "-princ") ;
-  princCurve = ipExistsArgument (argv, "-prinCurve") ;  
-  parFile = ipExistsArgument (argv, "-parFile") ;
-  phiThetaFile = ipExistsArgument (argv, "-phiTheta") ;
-  // make sure the arguments are valid
-  if ( !coefFileName || !baseName || ipExistsArgument(argv, "-help") )
+  
+   // make sure the arguments are valid
+  if ( !coefFileName || !meshName || ipExistsArgument(argv, "-help") )
   {
     cout << "Usage:" << endl ;
-    cout << "SPHARMTool -coef CoefficientFileName -output OutputFileBaseName [-level subdivisionLevel] [-deg spharmDegree] [-d] [-dd] [-normal] [-unit] [-I] [-II] [-kappa] [-c] [-s] [-h] [-k] [-princ] [-prinCurve] [-parFile] [-phiThetaFile]" << endl ;
+    cout << "SPHARMTool -coef CoefficientFileName -output outputmesh.vtk [-level subdivisionLevel] [-deg spharmDegree]" << endl ;
     return 0 ;
   }
   
@@ -86,15 +66,14 @@ int main(int argc, const char **argv)
   ITKVTKConverter->SetInput( mesh);
   vtkPolyData *SurfMesh;
   SurfMesh = ITKVTKConverter->GetOutput();
-  char meshName1[350] ;
-  sprintf (meshName1, "%s.vtk", baseName) ;
+ 
   // Writing the file
 #if VTK_MAJOR_VERSION > 5
   writer1->SetInputData(SurfMesh);
 #else
   writer1->SetInput(SurfMesh);
 #endif
-  writer1->SetFileName(meshName1 );
+  writer1->SetFileName(meshName );
   
   writer1->Write();
   return 0 ;
